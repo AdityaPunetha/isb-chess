7ec6bd81a85776ce76c7d994c23ab2e17636c79c
AdityaPunetha <adityapunetha6@gmail.com>
2021-06-11 17:20:34 +0530

    shessdotcom done and created functions

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import requests
 import json
 
-response = requests.get('https://lichess.org/api/user/apollo7701')
-json_data = json.loads(response.text)
-print(json_data['perfs']['blitz']['rating'], json_data['perfs']['rapid']['rating'])
+
+def get_lichess(name):
+    url = 'https://lichess.org/api/user/' + name
+    response = requests.get(url)
+    json_data = json.loads(response.text)
+    return json_data
+
+
+def get_chessdotcom(name):
+    url = 'https://api.chess.com/pub/player/{}/stats'.format(name)
+    response = requests.get(url)
+    json_data = json.loads(response.text)
+    return json_data
+
+
+print(get_lichess(""))
+print(get_chessdotcom(''))
